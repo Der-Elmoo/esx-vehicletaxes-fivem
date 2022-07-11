c1282c16b2acc5cde0b5d787b4afca7d18f3de8e
Der Elmo <jannik@staxrp.de>
2022-07-11 23:47:23 +0200

    Update config.lua

--- config.lua
+++ config.lua
@@ -1,3 +1,20 @@
 Config = {}
 
-Config.Tax = 50 --Tax for ONE car. Will be multiplied by the count of cars a user has
+Config.Locale = "en"
+Config.Time = 20 * 6000 --This is the time between the taxes. You can change the first number. 
+Config.Tax = 25 --Tax for ONE car. Will be multiplied by the count of cars a user has
+
+Translation = {
+    ['de'] = {
+        ['title'] = 'Finanzamt',
+        ['description'] = 'Du hast~r~ %s $~s~ für~y~ %s Fahrzeuge~s~ gezahlt.'
+    },
+    ['en'] = {
+        ['title'] = 'Tax Office',
+        ['description'] = 'You paid~r~ %s $~s~ for~y~ %s vehicles~s~.'
+    },
+    ['fr'] = {
+        ['title'] = 'Bureau des impôts',
+        ['description'] = 'Vous avez payé~r~ %s $~s~ de taxes pour~y~ %s véhicule~s~.'
+    }
+}
